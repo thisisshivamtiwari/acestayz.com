6536135f4ccf3985540e7960df8d20bd5d7edba4
shivam sharma <shiv.sharma4709@gmail.com>
2025-11-12 00:35:43 +0530

    fix: update script and stylesheet references in index.html; enhance Vite configuration for better hosting and preview support

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,19 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
+  server: {
+    host: '0.0.0.0',
+
+    allowedHosts: [
+      'www.acestayz.com',
+      'acestayz.com',
+    ]
+  },
+  preview: {
+    allowedHosts: ['acestayz.com', 'www.acestayz.com'],
+  },
+  define: {
+    'process.env.NODE_DEBUG': false,
+    global: {},
+  }
 })
